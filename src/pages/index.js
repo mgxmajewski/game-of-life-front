afab77c6f2776e0a57993dc1c4c2c8a93f385bee
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-07-24 20:38:13 +0200

    Create home.module.css and apply it to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
-import * as React from "react"
+import * as React from "react";
+import {Link} from "gatsby";
 import Layout from "../components/Layout";
+import {header, btn} from "../styles/home.module.css";
 
 const IndexPage = () => {
     return (
         <Layout>
-        <section>
+        <section className={header}>
             <div>
-                <h2>Hello Cat!</h2>
+                <h2>Convay's Game of Life</h2>
+                <h3>Discover Cellular Automata</h3>
+                <p>You can watch already discovered patterns or try to find new ones yourself.</p>
+                <Link className={btn} to="/grid">New Game</Link>
             </div>
         </section>
         </Layout>
