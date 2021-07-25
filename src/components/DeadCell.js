794de3b381ee9524d0b7fb0a2d67530f30dc93a3
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-07-25 22:39:00 +0200

    Replace svg canvas

---
+++ src/components/DeadCell.js
@@ -0,0 +1,27 @@
+// https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
+import React, { useRef, useEffect } from 'react'
+
+const DeadCell = props => {
+
+    const canvasRef = useRef(null)
+
+    const draw = ctx => {
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20, 0, 2*Math.PI)
+        ctx.fill()
+    }
+
+    useEffect(() => {
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+
+        //Our draw come here
+        draw(context)
+    }, [draw])
+
+    return <canvas ref={canvasRef} {...props}/>
+}
+
+export default DeadCell
