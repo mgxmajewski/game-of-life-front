66c19ff2deba4549cefb6dc21ec44fa86578a214
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-07-26 17:03:10 +0200

    Add plugin to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
+    "babel-plugin-styled-components",
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
