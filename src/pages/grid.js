43c95d3ab34bad2ad9e0894f3ed43bf50c66780e
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-07-27 03:11:20 +0200

    Add useMutations and hooks to send state to graphQL

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Layout from "../components/Layout";
 import {cellGrid} from "../styles/grid.module.css"
 import AliveCell from '../components/AliveCell.js'
 import DeadCell from '../components/DeadCell.js'
-import {gql, useSubscription} from "@apollo/client";
+import {gql, useMutation, useSubscription} from "@apollo/client";
 
 
 const GET_STATE = gql`
@@ -15,15 +15,44 @@ const GET_STATE = gql`
     }
 `;
 
+const SET_STATE = gql`
+    mutation ($user: String!, $grid:[[String]]!){
+        postState(user: $user, grid: $grid)
+    }
+`;
+
 const Grid = () => {
+    const [cell, setCell] = useState("")
+    useEffect(() => {
+        let x = Number(cell.charAt(0))
+        let y = Number(cell.charAt(2))
+        // stateOfGrid[`${x}`][`${y}`] = "#"
+        if(stateOfGrid){
+            stateOfGrid[x][y]= "#"
+            setStateOfGrid({
+                variables: {
+                    user: "Michal",
+                    grid: stateOfGrid
+                }
+            })
+        }
+
+        console.log(cell)
+    },[cell]);
+
+    const [setStateOfGrid] = useMutation(SET_STATE);
+
+    const onUpdate = (e) => {
+        setCell(e.target.id)
+    }
+
     const { data } = useSubscription(GET_STATE);
     if (!data) {
         return null;
     }
 
-
-    const state = data.states[data.states.length-1].grid
-    let numberOfColumns = state[0].length
+    const stateOfGrid = data.states[data.states.length-1].grid
+    let numberOfColumns = stateOfGrid[0].length
 
     const columnParser = () => {
         let columns = ''
@@ -41,15 +70,15 @@ const Grid = () => {
     return (
         <Layout>
             <div>
-                {state.map((rows, x) => {
+                {stateOfGrid.map((rows, x) => {
                     return (
                         <div style={divStyle} key={x}>
                             {rows.map((cell, y) => {
-                                const cellCoordinates = `${y.toString()},${x.toString()}`
-                                return <div className={cellGrid} key={cellCoordinates}>{
+                                const cellCoordinates = `${x},${y}`
+                                return <div  className={cellGrid} key={cellCoordinates}>{
                                     (cell === "#")
-                                        ? <AliveCell />
-                                        : <DeadCell/>
+                                        ? <AliveCell id={cellCoordinates} onClick={(e) => onUpdate(e)}/>
+                                        : <DeadCell id={cellCoordinates} onClick={(e) => onUpdate(e)}/>
                                 }</div> ;
                             })}
                         </div>
