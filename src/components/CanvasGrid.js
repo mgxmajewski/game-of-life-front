0b839ad321596ffbf4fdde9d8b76b0768d120612
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-08-19 23:35:58 +0200

    Render Grid of Cells using dummyState

--- src/components/CanvasGrid.js
+++ src/components/CanvasGrid.js
@@ -1,21 +1,38 @@
 import React, { useRef, useEffect } from 'react'
+import {dummyState} from "../utils/InitialState";
+import {Cell} from "../canvas/Cell";
 
 const CanvasGrid = props => {
 
     const canvasRef = useRef(null)
 
+    const gridState = dummyState
+
     const draw = (ctx, canvas) => {
+
         const width = canvas.width
         const height = canvas.height
-        ctx.fillStyle = '#fc0000'
+        ctx.fillStyle = '#5c5c5c'
         ctx.fillRect(0,0,width, height)
         ctx.fill()
+        console.log(gridState)
+        let xShift = 0
+        let yShift = 0
+        const cellWidth = width/gridState[0].length
+        const cellRadius = cellWidth/2
+        console.log(cellWidth)
+        for(let col = 0; col<gridState[0].length; col++) {
+            for(let row = 0; row<gridState.length; row++) {
+                new Cell(ctx, cellRadius + xShift, cellRadius + yShift, cellRadius, '#000127FF').draw()
+                xShift+=cellWidth
+            }
+            xShift=0
+            yShift+=cellWidth
+        }
     }
 
     useEffect(() => {
         const canvas = canvasRef.current
-        canvas.style.width = '200px';
-        canvas.style.heigth = '200px';
         canvas.width  = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
         const context = canvas.getContext('2d')
