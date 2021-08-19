649b26d488b6e0ea50e0845fa090ee0e7b368767
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-08-19 23:34:42 +0200

    Create Cell canvas drawing function

---
+++ src/canvas/Cell.js
@@ -0,0 +1,15 @@
+export function Cell(ctx, x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.radians = 0;
+
+    this.draw = () => {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
