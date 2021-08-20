59f998b8db92999a5bcd115b1a3dbc09a9f2c6bb
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-08-20 17:38:26 +0200

    Provide grid state from apollo client and pass it to CanvasGrid component via props

--- src/pages/canvasgrid.js
+++ src/pages/canvasgrid.js
@@ -1,14 +1,35 @@
 import * as React from "react";
-import {Link} from "gatsby";
 import Layout from "../components/Layout";
-import {header, btn} from "../styles/home.module.css";
 import CanvasGrid from "../components/CanvasGrid";
+import {dummyState} from "../utils/InitialState";
+import {gql, useSubscription} from "@apollo/client";
+
+const GET_STATE = gql`
+    subscription {
+        states{
+            id
+            grid
+        }
+    }
+`;
 
 const CanvasGridPage = () => {
+
+    const {loading, error,data} = useSubscription(GET_STATE);
+
+    // Handle subscription loading and error
+    if (loading) return <p>Waiting for Server Response</p>
+    if (error) return <p>Server Down</p>
+
+    // Handle case when there is no initial state sent by graphQL
+    const graphQLInitialData = data.states[data.states.length-1]
+    const isGraphQlDefined = graphQLInitialData !== undefined
+    const stateOfGrid = isGraphQlDefined ? graphQLInitialData.grid : dummyState
+
     return (
         <Layout>
             <div>
-                <CanvasGrid/>
+                <CanvasGrid state={stateOfGrid}/>
             </div>
         </Layout>
     )
