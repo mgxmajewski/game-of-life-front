a3c7ae347d7308b0166ddd6e246089c7fa5ebd69
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-09-12 21:39:12 +0200

    Rename canvas grid to grid (delete old grid)

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import * as React from "react";
 import Layout from "../components/Layout";
-import {cellGrid, gridDiv, columnBtn, rowBtn} from "../styles/grid.module.css"
-import AliveCell from '../components/AliveCell.js'
-import DeadCell from '../components/DeadCell.js'
+import CanvasGrid from "../components/CanvasGrid";
+import {dummyState} from "../utils/InitialState";
 import {gql, useMutation, useSubscription} from "@apollo/client";
-import {dynamicColumns, divGridStyle} from '../utils/DynamicColumns'
+import {useEffect, useState} from "react";
 import {frameModHandler} from "../utils/FrameModHandler";
-import {dummyState} from "../utils/InitialState"
+import {getLongestRow} from "../utils/GetLongestRow";
+import {coordinatesBtn} from "../styles/grid.module.css"
 
 const GET_STATE = gql`
     subscription {
@@ -23,72 +23,81 @@ const SET_STATE = gql`
     }
 `;
 
-const Grid = () => {
+const CanvasGridPage = () => {
     const [cell, setCell] = useState("")
     const [setStateOfGrid] = useMutation(SET_STATE);
     const [isModified, setIsModified] = useState(false)
+    const [areCoordinatesToDisplay, setareCoordinatesToDisplay] = useState(false)
 
     // Add handler to flip state of the cell (to work from the first click)
     useEffect(()=> {
         frameModHandler(stateOfGrid, setStateOfGrid, cell);
     }, [cell])
 
-    // Create useSubscription to get realtime state of graphql
     const {loading, error,data} = useSubscription(GET_STATE);
 
     // Handle subscription loading and error
     if (loading) return <p>Waiting for Server Response</p>
     if (error) return <p>Server Down</p>
 
-
     // Handle case when there is no initial state sent by graphQL
     const graphQLInitialData = data.states[data.states.length-1]
     const isGraphQlDefined = graphQLInitialData !== undefined
     const stateOfGrid = isGraphQlDefined ? graphQLInitialData.grid : dummyState
 
-    // Dynamically add right number of columns in cell grid
-    const numberOfColumns = stateOfGrid[0].length
-    const columnsToRender = dynamicColumns(numberOfColumns)
-    const addColumns = divGridStyle(columnsToRender)
 
-    // Handles user changes to grid
-    let onUpdate =(e) => {
-        setIsModified(!isModified)
-        setCell(`${e.target.id},${isModified}`)
+    const isCell = (x, y) => {
+        if (y <= stateOfGrid.length - 1){
+            return x <= stateOfGrid[y].length - 1;
+        }
+    }
+
+    const toggleDisplayCoordinates = () => {
+        console.log(areCoordinatesToDisplay)
+        setareCoordinatesToDisplay(!areCoordinatesToDisplay)
+    }
+
+    const calculateWhichCellClicked = e => {
+        const width = e.target.width
+        const gridState = stateOfGrid
+        let cellWidth;
+        let cellHeight;
+        cellWidth = cellHeight = width/getLongestRow(gridState)
+        const rect = e.target.getBoundingClientRect()
+        const x = Math.floor((e.clientX - rect.left)/cellWidth)
+        const y = Math.floor((e.clientY - rect.top)/cellHeight)
+        if(isCell(x,y)){
+            return [x,y]
+        }
+    }
+
+    const changeCellState = e => {
+        const coordinates = calculateWhichCellClicked(e)
+        if (coordinates) {
+            const x = coordinates[0]
+            const y = coordinates[1]
+            setIsModified(!isModified)
+            setCell(`${y},${x},${isModified}`)
+        }
     }
 
     return (
         <Layout>
-            <button className={rowBtn}>+ ROW</button>
-            <button className={rowBtn}>- ROW</button>
-            <button className={columnBtn}>+ COLUMN</button>
-            <button className={columnBtn}>- COLUMN</button>
-            <div className={gridDiv}>
-                {stateOfGrid.map((rows, x) => {
-                    return (
-                        <div style={addColumns} key={x}>
-                            {rows.map((cell, y) => {
-                                const cellCoordinates = `${x},${y}`
-                                return (
-                                    <div key={cellCoordinates}
-                                         className={cellGrid}>
-                                        {(cell === "#")
-                                            ? <AliveCell id={cellCoordinates} onClick={(e) => onUpdate(e)}/>
-                                            : <DeadCell id={cellCoordinates} onClick={(e) => onUpdate(e)}/>
-                                        }
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    );
-                })}
+            <div>
+                <button
+                    className={coordinatesBtn}
+                    onClick={() => toggleDisplayCoordinates()}
+                >
+                    Show Coordinates
+                </button>
+                <CanvasGrid
+                    state={stateOfGrid}
+                    coordinates={areCoordinatesToDisplay.toString()}
+                    onMouseDown={(e) => changeCellState(e)}
+                />
             </div>
-            <button className={columnBtn}>- COLUMN</button>
-            <button className={columnBtn}>+ COLUMN</button>
-            <button className={rowBtn}>- ROW</button>
-            <button className={rowBtn}>+ ROW</button>
         </Layout>
     )
 }
 
-export default Grid
+export default CanvasGridPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
                 <h2>Convay's Game of Life</h2>
                 <h3>Discover Cellular Automata</h3>
                 <p>You can watch already discovered patterns or try to find new ones yourself.</p>
-                <Link className={btn} to="/canvasgrid">New Game</Link>
+                <Link className={btn} to="/grid">New Game</Link>
             </div>
         </section>
         </Layout>
