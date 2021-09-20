0f0034e53dfa0cae7941de8928b66a493f4cafba
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-09-20 22:57:57 +0200

    Temporarily comment gridMutation part of FrameModHandler.js

--- src/utils/FrameModHandler.js
+++ src/utils/FrameModHandler.js
@@ -1,29 +1,29 @@
-/** Handles changes made by user to grid during frame
- * @param grid
- * @param {(options?: MutationFunctionOptions<*, OperationVariables>) => Promise<FetchResult<*>>} gridMutation
- * @param {string} cell
- */
+// /** Handles changes made by user to grid during frame
+//  * @param grid
+//  * @param {(options?: MutationFunctionOptions<*, OperationVariables>) => Promise<FetchResult<*>>} gridMutation
+//  * @param {string} cell
+//  */
 
-export const frameModHandler = (grid, gridMutation, cell) => {
+export const frameModHandler = (grid, cell) => {
     let x = Number(cell.split(",")[0])
     let y = Number(cell.split(",")[1])
     if (grid){
         const isAlive = grid[x][y] === "#"
         grid[x][y] = isAlive ? "_" : "#";
-        postFrameMod(grid, gridMutation)
+        // postFrameMod(grid, gridMutation)
     }
 }
 
-/** Handles sending mutation to graphQL with updated grid
- * @param {function(MutationFunctionOptions<*, OperationVariables>=): Promise<FetchResult<*>>} gridMutation
- */
-const postFrameMod = (grid, gridMutation) => {
-    gridMutation({
-        variables: {
-            user: "Michal",
-            grid: grid
-        }
-    })
-}
+// /** Handles sending mutation to graphQL with updated grid
+//  * @param {function(MutationFunctionOptions<*, OperationVariables>=): Promise<FetchResult<*>>} gridMutation
+//  */
+// const postFrameMod = (grid, gridMutation) => {
+//     gridMutation({
+//         variables: {
+//             user: "Michal",
+//             grid: grid
+//         }
+//     })
+// }
 
 
