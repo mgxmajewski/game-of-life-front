0e181e57884049f2c3d466e1ffaedb7779dd2dcc
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-09-20 22:52:41 +0200

    Use wrapPageElement on Layout component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-export { wrapRootElement } from './src/apollo/wrap-root-element'
+import React from 'react';
+import Layout from "./src/components/Layout";
+export { wrapRootElement } from './src/apollo/wrap-root-element';
+
+export const wrapPageElement = ({ element, props }) => {
+    return <Layout {...props}>{element}</Layout>;
+};
--- gatsby-srr.js
+++ gatsby-srr.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import Layout from "./src/components/Layout";
+
 export { wrapRootElement } from './src/apollo/wrap-root-element';
+
+export const wrapPageElement = ({ element, props }) => {
+    return <Layout {...props}>{element}</Layout>;
+};
