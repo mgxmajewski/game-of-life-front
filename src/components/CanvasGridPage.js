a855d8d1385fe7e1e74d92332b14079ab9d82d24
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-09-26 01:55:11 +0200

    Add handlers to support "drawing"(flipping state) on multiple cells while clicking once.

--- src/components/CanvasGridPage.js
+++ src/components/CanvasGridPage.js
@@ -5,11 +5,18 @@ import {frameModHandler} from "../utils/FrameModHandler";
 import {getLongestRow} from "../utils/GetLongestRow";
 import {coordinatesBtn} from "../styles/grid.module.css"
 
+const getNullCoords = () => new Array(2).fill(null)
+
+let nextHover = getNullCoords()
+let currentHover = getNullCoords()
+let firstClick = getNullCoords()
+
 const CanvasGridPage = props => {
     const apolloGrid = props.initialgrid
     const [areCoordinatesToDisplay, setAreCoordinatesToDisplay] = useState(false)
+    const [isClicked, setIsClicked] = useState(false)
 
-    const isCell = (x, y) => {
+    const isCell = ([x, y]) => {
         if (y <= apolloGrid.length - 1){
             return x <= apolloGrid[y].length - 1;
         }
@@ -19,24 +26,49 @@ const CanvasGridPage = props => {
         setAreCoordinatesToDisplay(!areCoordinatesToDisplay)
     }
 
-    const calculateWhichCellClicked = e => {
-        const width = e.target.width
+    const getBounding = (e) => e.target.getBoundingClientRect()
+    const getWidth = (e) => e.target.width
+    const deepClone = value => JSON.stringify(value)
+
+
+    const getCoordinates = (e) => {
+        const width = getWidth(e)
         let cellWidth;
         let cellHeight;
         cellWidth = cellHeight = width/getLongestRow(apolloGrid)
-        const rect = e.target.getBoundingClientRect()
+        const rect = getBounding(e)
         const x = Math.floor((e.clientX - rect.left)/cellWidth)
         const y = Math.floor((e.clientY - rect.top)/cellHeight)
-        if(isCell(x,y)){
-            return [x,y]
+        return [x,y]
+    }
+
+    const calculateWhichCellClicked = e => {
+        if(isCell(getCoordinates(e))){
+            firstClick = getCoordinates(e)
+        }
+    }
+
+    const calculateWhichCellHovered = e => {
+        if (!isClicked){
+            return
+        }
+        currentHover = getCoordinates(e)
+        const isAlreadyClicked = deepClone(currentHover) === deepClone(firstClick)
+        const isAlreadyHovered = deepClone(currentHover) === deepClone(nextHover)
+        if(!isAlreadyClicked && !isAlreadyHovered){
+            console.log(`nextHover: ${nextHover}`)
+            nextHover = getCoordinates(e)
+            frameModHandler(apolloGrid, nextHover)
         }
     }
 
     const changeCellState = e => {
-        e.target.getBoundingClientRect()
-        const coordinates = calculateWhichCellClicked(e)
-        frameModHandler(apolloGrid, coordinates)
+        setIsClicked(true)
+        calculateWhichCellClicked(e)
+        frameModHandler(apolloGrid, firstClick)
     }
+
+    const endStroke = () => setIsClicked(false)
 
     return (
         <div>
@@ -49,7 +81,10 @@ const CanvasGridPage = props => {
             <CanvasGrid
                 state={apolloGrid}
                 coordinates={areCoordinatesToDisplay.toString()}
+                onMouseUp={endStroke}
                 onMouseDown={changeCellState}
+                onMouseMove={calculateWhichCellHovered}
+                onMouseOut={endStroke}
             />
         </div>
     )
