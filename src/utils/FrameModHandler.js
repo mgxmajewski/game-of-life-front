9f8704b6bf55d1d18813ec2f787d28ab5079b5e4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-09-27 00:10:59 +0200

    Remove no-cors from fetch requestOptions and add dummy authorization token.

--- src/utils/FrameModHandler.js
+++ src/utils/FrameModHandler.js
@@ -3,8 +3,9 @@
  * @param coordinates
  */
 
-export const frameModHandler = (grid, coordinates) => {
-    const myHeaders = new Headers();
+export const frameModHandler = async (grid, coordinates) => {
+    const myHeaders = await new Headers();
+    myHeaders.append("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFudGhvbnkgVmFsaWQgVXNlciIsImlhdCI6MTQyNTQ3MzUzNX0.KA68l60mjiC8EXaC2odnjFwdIDxE__iDu5RwLdN1F2A");
     myHeaders.append("Content-Type", "application/json");
 
     const raw = JSON.stringify({
@@ -14,8 +15,7 @@ export const frameModHandler = (grid, coordinates) => {
 
     const requestOptions = {
         method: 'POST',
-        mode: "no-cors",
-        headers: myHeaders,
+        headers:  myHeaders,
         body: raw,
         redirect: 'follow'
     };
