c0e071eeca84f8977bad677fdc56926a35b384f6
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-01 11:29:08 +0100

    Auto format.

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,58 +14,58 @@ const gatsbyRequiredRules = path.join(
 );
 
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "game-of-life-front",
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "babel-plugin-styled-components",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-CBY2FFEHZK",
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "game-of-life-front",
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-      {
-     resolve: "gatsby-plugin-eslint",
-     options: {
-       // Gatsby required rules directory
-       rulePaths: [gatsbyRequiredRules],
-       // Default settings that may be ommitted or customized
-       stages: ["develop"],
-       extensions: ["js", "jsx", "ts", "tsx"],
-       exclude: ["node_modules", "bower_components", ".cache", "public"],
-       // Any additional eslint-webpack-plugin options below
-       // ...
-     },
-   },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/ // See below to configure properly
-        }
-      }
-    },
-  ],
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "babel-plugin-styled-components",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "G-CBY2FFEHZK",
+            },
+        },
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-plugin-eslint",
+            options: {
+                // Gatsby required rules directory
+                rulePaths: [gatsbyRequiredRules],
+                // Default settings that may be ommitted or customized
+                stages: ["develop"],
+                extensions: ["js", "jsx", "ts", "tsx"],
+                exclude: ["node_modules", "bower_components", ".cache", "public"],
+                // Any additional eslint-webpack-plugin options below
+                // ...
+            },
+        },
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /assets/ // See below to configure properly
+                }
+            }
+        },
+    ],
 };
