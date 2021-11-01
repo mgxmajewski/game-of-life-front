5a934cb23aec5cc23d55a71ff0d88b3097810ee4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-01 10:30:29 +0100

    Change ' to escape symbol &apos;

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
     return (
         <section className={header}>
             <div>
-                <h2>Convay's Game of Life</h2>
+                <h2>Convay&apos;s Game of Life</h2>
                 <h3>Discover Cellular Automata</h3>
                 <p>You can watch already discovered patterns or try to find new ones yourself.</p>
                 <Link className={btn} to="/grid">New Game</Link>
