43949d625188d838830bfdb652670915a1cfd2db
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-03 11:29:13 +0100

    Create gatsby-node config.

---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+exports.onCreateWebpackConfig = ({
+                                     actions
+                                 }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            fallback: {
+                "fs": false,
+                "tls": false,
+                "net": false,
+                "path": false,
+                "zlib": false,
+                "http": false,
+                "https": false,
+                "stream": false,
+                "crypto": false,
+                "crypto-browserify": false
+            }
+        }
+    });
+}
