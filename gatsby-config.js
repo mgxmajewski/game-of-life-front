f395f4d80ec54f856317594eb73de9109cecff30
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-06 16:44:31 +0100

    Remove unused gatsby plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,40 +4,5 @@ module.exports = {
         title: "game-of-life-front",
     },
     plugins: [
-        "gatsby-plugin-sass",
-        "gatsby-plugin-image",
-        "babel-plugin-styled-components",
-        {
-            resolve: "gatsby-plugin-google-analytics",
-            options: {
-                trackingId: "G-CBY2FFEHZK",
-            },
-        },
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
-        {
-            resolve: "gatsby-plugin-manifest",
-            options: {
-                icon: "src/images/icon.png",
-            },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "images",
-                path: "./src/images/",
-            },
-            __key: "images",
-        },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                rule: {
-                    include: /assets/ // See below to configure properly
-                }
-            }
-        },
     ],
 };
