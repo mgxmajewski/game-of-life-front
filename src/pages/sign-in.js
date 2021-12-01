7fc76ae571af4022317560edac6f7c400400e038
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-01 22:04:48 +0100

    Redirect if user to grid if is already logged in.

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,51 +1,52 @@
 import React from "react"
-import { navigate } from "gatsby"
-import { handleLogin, isLoggedIn } from "../utils/LoginHandler"
-class Login extends React.Component {
-  state = {
-    email: ``,
-    password: ``,
-  }
-  handleUpdate = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    })
-  }
+import {navigate} from "gatsby";
+import {handleLogin, isLoggedIn} from "../utils/LoginHandler"
 
-  handleSubmit = event => {
-    event.preventDefault()
-    handleLogin(this.state)
-  }
-  render() {
-    // if (isLoggedIn()) {
-    //   navigate(`/grid`)
-    // }
-    return (
-      <>
-        <h1>Log in</h1>
-        <form
-          method="post"
-          onSubmit={event => {
-            this.handleSubmit(event)
-            // navigate(`/grid`)
-          }}
-        >
-          <label>
-            email
-            <input type="text" name="email" onChange={this.handleUpdate} />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              name="password"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <input type="submit" value="Log In" />
-        </form>
-      </>
-    )
-  }
+class Login extends React.Component {
+    state = {
+        email: ``, password: ``,
+    }
+    handleUpdate = event => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        handleLogin(this.state)
+    }
+
+    render() {
+        if (isLoggedIn()) {
+            navigate(`/grid`)
+        }
+        return (
+            <>
+                <h1>Log in</h1>
+                <form
+                    method="post"
+                    onSubmit={event => {
+                        this.handleSubmit(event)
+                    }}
+                >
+                    <label>
+                        email
+                        <input type="text" name="email" onChange={this.handleUpdate}/>
+                    </label>
+                    <label>
+                        Password
+                        <input
+                            type="password"
+                            name="password"
+                            onChange={this.handleUpdate}
+                        />
+                    </label>
+                    <input type="submit" value="Log In"/>
+                </form>
+            </>
+        )
+    }
 }
+
 export default Login
