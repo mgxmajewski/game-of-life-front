f534a2c78601ca3cb255154086c51d078a635141
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-02 21:18:33 +0100

    Add hellouser widget with conditional login nav elements.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Link} from "gatsby";
+import {userMailNameVar} from "../utils/cache";
 
 const NavBar = () => {
+    console.log(`userMailNameVar(): ` + userMailNameVar().length);
+    const userLogged = userMailNameVar().length !== 0;
     return (
         <nav>
             <h1>AgileCat.io</h1>
             <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/grid">Grid</Link>
-                <Link to="/sign-in">Login</Link>
+                {/*<Link to="/">Home</Link>*/}
+                {/*<Link to="/grid">Grid</Link>*/}
+                {userLogged ?
+                    <React.Fragment>
+                        <span className="hellouser">Hi, {userMailNameVar()}!</span>
+                        <Link className="signout" to="/">Sign Out</Link>
+                    </React.Fragment>
+                    :
+                    <React.Fragment>
+                        <Link to="/sign-in">Login</Link>
+                    </React.Fragment>
+                }
             </div>
         </nav>
     );
