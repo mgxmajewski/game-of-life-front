7de8a1682b96f2323e3fe968c659bb67bbf4b76a
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-02 21:45:47 +0100

    Add condition for "New Game" button.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import {Link} from "gatsby";
 import {header} from "../styles/home.module.css";
 import {btn} from "../styles/global.css";
+import {isLoggedIn} from "../utils/LoginHandler";
 
 
 const IndexPage = () => {
@@ -11,7 +12,10 @@ const IndexPage = () => {
                 <h2>Conway&apos;s Game of Life</h2>
                 <h3>Discover Cellular Automata</h3>
                 <p>You can watch already discovered patterns or try to find new ones yourself.</p>
-                <Link className="btn" to="/sign-in">New Game</Link>
+                {isLoggedIn()
+                    ? <Link className="btn" to="/grid">New Game</Link>
+                    : <Link className="btn" to="/sign-in">New Game</Link>
+                }
             </div>
         </section>
     )
