94f45cb12e19161a214f41796d523eb1ceff0253
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-06 23:21:08 +0100

    Remove conditional link in index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import {Link} from "gatsby";
 import {header} from "../styles/home.module.css";
 import {btn} from "../styles/global.css";
-import {isLoggedIn} from "../utils/LoginHandler";
 
 
 const IndexPage = () => {
@@ -12,10 +11,7 @@ const IndexPage = () => {
                 <h2>Conway&apos;s Game of Life</h2>
                 <h3>Discover Cellular Automata</h3>
                 <p>You can watch already discovered patterns or try to find new ones yourself.</p>
-                {isLoggedIn()
-                    ? <Link className="btn" to="/grid">New Game</Link>
-                    : <Link className="btn" to="/sign-in">New Game</Link>
-                }
+                <Link className="btn" to="/grid">New Game</Link>
             </div>
         </section>
     )
