e1ce2226baa7b3756db7caa670883a474ec6512d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-06 23:25:56 +0100

    Fix AuthSync to fetch for fresh token. Seems that importing async/await function from separate module doesn't work in reliable way. Temporarily comment fetchFreshToken.js

--- src/components/AuthSync.js
+++ src/components/AuthSync.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from "prop-types";
 import {isLoggedIn} from "../utils/LoginHandler";
 import {navigate} from "gatsby";
+// import {fetchFreshToken} from "../utils/fetchFreshToken";
+import {authenticatedToken, userIdVar} from "../utils/cache";
+import jwt_decode from "jwt-decode";
+import {wsClient} from "../apollo/client";
+const fetch = require(`node-fetch`)
+
+const isToken = () => {
+    const token = authenticatedToken()[0]
+    return !!token
+}
 
 
 const AuthSync = (props) => {
-            if (!isLoggedIn()) {
-            navigate(`/sign-in`)
-        }
-    return (<>
-        {props.children}
-    </>
+    const [isFetching, setIsFetching] = useState(true)
+
+     useEffect(() => {
+    // You need to restrict it at some point
+    // This is just dummy code and should be replaced by actual
+    if (!isToken()) {
+        asyncToken();
+    } else {
+        setIsFetching(false)
+    }
+  }, []);
+
+    const asyncToken = async () => {
+
+    const myHeaders = {
+        "Content-Type": "application/json"
+    };
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        redirect: 'follow',
+        credentials: 'include'
+    };
+
+    fetch("http://localhost:3000/user/refresh-token", requestOptions)
+        .then(response => {
+            if (response.status === 401) {
+                // authenticatedToken([])
+                navigate(`/sign-in`)
+            }
+            return response
+        })
+        .then(response => response.text())
+        .then((response) => authenticatedToken([response]))
+        .then(() => userIdVar([jwt_decode(authenticatedToken()[0]).id]))
+        .then(() => console.log(userIdVar()))
+        .then(() => wsClient.close(true))
+        .then(()=> setIsFetching(false))
+        .catch(error => console.log('Fetch error', error));
+    }
+    return (
+        <>
+            {isFetching
+                ? <p>loading</p>
+                : <> {props.children} </>
+            }
+        </>
     );
 };
 
--- src/utils/fetchFreshToken.js
+++ src/utils/fetchFreshToken.js
@@ -1,33 +1,37 @@
-import {authenticatedToken} from "./cache"
-import {navigate} from "gatsby";
-
-const fetch = require(`node-fetch`)
-
-export const fetchFreshToken = async () => {
-
-    const myHeaders = {
-        "Content-Type": "application/json"
-    };
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        // redirect: 'follow',
-        credentials: 'include'
-    };
-
-    fetch("http://localhost:3000/user/refresh-token", requestOptions)
-        .then(response => {
-            if (response.status === 401) {
-                // authenticatedToken([])
-                navigate(`/sign-in`)
-            }
-            return response
-        })
-        .then(response => response.text())
-        .then((response) => authenticatedToken([response]))
-        .then((text) => console.log(text))
-        .catch(error => console.log('Fetch error', error));
-}
-
-
+// import {authenticatedToken, userIdVar} from "./cache"
+// import {navigate} from "gatsby";
+// import jwt_decode from "jwt-decode";
+// import {wsClient} from "../apollo/client";
+//
+// const fetch = require(`node-fetch`)
+//
+// export const fetchFreshToken = async () => {
+//
+//     const myHeaders = {
+//         "Content-Type": "application/json"
+//     };
+//
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: myHeaders,
+//         redirect: 'follow',
+//         credentials: 'include'
+//     };
+//
+//     fetch("http://localhost:3000/user/refresh-token", requestOptions)
+//         .then(response => {
+//             if (response.status === 401) {
+//                 // authenticatedToken([])
+//                 navigate(`/sign-in`)
+//             }
+//             return response
+//         })
+//         .then(response => response.text())
+//         .then((response) => authenticatedToken([response]))
+//         .then(() => userIdVar([jwt_decode(authenticatedToken()[0]).id]))
+//         .then(() => console.log(userIdVar()))
+//         // .then(() => wsClient.close(true))
+//         .catch(error => console.log('Fetch error', error));
+// }
+//
+//
