4d7c1e5543d6ce061d39139f335dfbdf8f23d278
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-08 01:34:22 +0100

    Fix subMinutes and isValidLongerThanMinute to check if token is close to expiry.

--- src/components/AuthSync.js
+++ src/components/AuthSync.js
@@ -1,60 +1,70 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from "prop-types";
-import {isLoggedIn} from "../utils/LoginHandler";
 import {navigate} from "gatsby";
 // import {fetchFreshToken} from "../utils/fetchFreshToken";
 import {authenticatedToken, userIdVar} from "../utils/cache";
 import jwt_decode from "jwt-decode";
 import {wsClient} from "../apollo/client";
+
 const fetch = require(`node-fetch`)
+
 
 const isToken = () => {
     const token = authenticatedToken()[0]
     return !!token
 }
 
+const subMinutes = function (dt, minutes) {
+    return new Date(dt.getTime() - minutes * 60000);
+}
+
+const isValidLongerThanMinute = (token) => {
+    const token_expiry = jwt_decode(token).exp * 1000
+    return subMinutes(new Date(token_expiry), 1) >=
+        new Date()
+}
 
 const AuthSync = (props) => {
     const [isFetching, setIsFetching] = useState(true)
 
-     useEffect(() => {
-    // You need to restrict it at some point
-    // This is just dummy code and should be replaced by actual
-    if (!isToken()) {
-        asyncToken();
-    } else {
-        setIsFetching(false)
-    }
-  }, []);
+    useEffect(() => {
+        // You need to restrict it at some point
+        // This is just dummy code and should be replaced by actual
+        if (!isToken()) {
+            asyncToken();
+        } else {
+            setIsFetching(false)
+        }
+    }, []);
 
     const asyncToken = async () => {
 
-    const myHeaders = {
-        "Content-Type": "application/json"
-    };
+        const myHeaders = {
+            "Content-Type": "application/json"
+        };
 
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        redirect: 'follow',
-        credentials: 'include'
-    };
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            redirect: 'follow',
+            credentials: 'include'
+        };
 
-    fetch("http://localhost:3000/user/refresh-token", requestOptions)
-        .then(response => {
-            if (response.status === 401) {
-                // authenticatedToken([])
-                navigate(`/sign-in`)
-            }
-            return response
-        })
-        .then(response => response.text())
-        .then((response) => authenticatedToken([response]))
-        .then(() => userIdVar([jwt_decode(authenticatedToken()[0]).id]))
-        .then(() => console.log(userIdVar()))
-        .then(() => wsClient.close(true))
-        .then(()=> setIsFetching(false))
-        .catch(error => console.log('Fetch error', error));
+        fetch("http://localhost:3000/user/refresh-token", requestOptions)
+            .then(response => {
+                if (response.status === 401) {
+                    // authenticatedToken([])
+                    navigate(`/sign-in`)
+                }
+                return response
+            })
+            .then(response => response.text())
+            .then((response) => authenticatedToken([response]))
+            .then(() => userIdVar([jwt_decode(authenticatedToken()[0]).id]))
+            .then(() => console.log(userIdVar()))
+            .then(() => wsClient.close(true))
+            .then(() => setIsFetching(false))
+            .catch(error => console.log('Fetch error', error));
     }
     return (
         <>
@@ -66,8 +76,8 @@ const AuthSync = (props) => {
     );
 };
 
-// AuthSync.propTypes = {
-//     children: PropTypes.node
-// };
+AuthSync.propTypes = {
+    children: PropTypes.node
+};
 
 export default AuthSync;
