8e7ed20dfcc4f432adc4831734d4abbdfbcd91be
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-20 23:25:46 +0100

    Create BottomButtons module.

---
+++ src/components/BottomButtons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/buttons.module.css'
+const BottomButtons = () => {
+    return (
+        <div className="bottom-grid-controller">
+            <button className="btn insert-first__col" type="submit" value="add column">
+                add column
+            </button>
+            <button className="btn delete-first__col" type="submit" value="add column"> - delete column ></button>
+            <button className="btn insert-last__col" type="submit" value="add column">
+                delete column
+                -
+            </button>
+            <button className="btn delete-last__col" type="submit" value="add column"> + add column</button>
+        </div>
+    );
+}
+
+export default BottomButtons
--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import GridProvider from "../components/GridProvider";
 import AuthSync from "../components/AuthSync";
+import BottomButtons from "../components/BottomButtons";
 
 const CanvasGridPageWrapped = () => {
     return (
         <AuthSync>
             <GridProvider/>
+            <BottomButtons/>
         </AuthSync>
     )
 }
