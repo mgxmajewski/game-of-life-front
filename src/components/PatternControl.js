857dcb214fc75e4b671bfe7f597d7b06d1010d01
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-29 23:31:32 +0100

    Add handler to load pattern by its primary key.

--- src/components/PatternControl.js
+++ src/components/PatternControl.js
@@ -8,14 +8,40 @@ import {navigate} from "gatsby";
 
 const PatternControl = () => {
 
-    const apiRoute = 'all-patterns/get'
-    const currentToken = useReactiveVar(authenticatedToken)
-    const [patternNames, setPatternNames] = useState()
-    const [isFetching, setIsFetching] = useState(true)
+    const apiRoute = 'all-patterns/get';
+    const currentToken = useReactiveVar(authenticatedToken);
+    const [patternNames, setPatternNames] = useState();
+    const [isFetching, setIsFetching] = useState(true);
+    const [patternToLoad, setPatternToLoad] = useState('0');
 
     useEffect(() => {
         fetchPattern()
     }, [])
+
+    const loadPattern = (e) => {
+        e.preventDefault()
+        const myHeaders = {
+            "Authorization": `${currentToken}`,
+            "Content-Type": "application/json"
+        };
+
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            credentials: 'include'
+        };
+
+        fetch(`http://localhost:3000/pattern/${patternToLoad}`, requestOptions)
+            .then(response => {
+                if (response.status === 401) {
+                    authenticatedToken([])
+                    navigate(`/sign-in`)
+                }
+                return response
+            })
+            .then(response => response.json())
+            .catch(error => console.log('Fetch error', error));
+    }
 
     const fetchPattern = async () => {
 
@@ -46,10 +72,19 @@ const PatternControl = () => {
 
     return (
         <div className="pattern-container">
-            <button className={coordinatesBtn}>Load Pattern</button>
+            <button
+                className={coordinatesBtn}
+                onClick={(e)=> loadPattern(e)}
+            >
+                Load Pattern
+            </button>
             {isFetching
                 ? <p>Loading</p>
-                : <select className="pattern-select" size="3">
+                : <select
+                    className="pattern-select"
+                    size="3"
+                    onChange={e => setPatternToLoad(e.target.value)}
+                >
                     {patternNames.listPatterns.map((option) => (
                         <option value={option.id}>{option.patternName}</option>
                     ))}
