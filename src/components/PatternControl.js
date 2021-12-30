1361e871cff410b21f7199a3816c5884ab9f37d4
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-12-30 14:03:54 +0100

    Create custom useFetch hook and fetchHandler to extract fetching logic to separate component.

--- src/components/PatternControl.js
+++ src/components/PatternControl.js
@@ -1,92 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import '../styles/pattern.css'
 import {coordinatesBtn} from "../styles/grid.module.css";
 import {useReactiveVar} from "@apollo/client";
+import {fetchHandler, useFetch} from "../utils/useFetch";
 import {authenticatedToken} from "../utils/cache";
-import {navigate} from "gatsby";
 
 
 const PatternControl = () => {
 
-    const apiRoute = 'all-patterns/get';
     const currentToken = useReactiveVar(authenticatedToken);
-    const [patternNames, setPatternNames] = useState();
-    const [isFetching, setIsFetching] = useState(true);
     const [patternToLoad, setPatternToLoad] = useState('0');
 
-    useEffect(() => {
-        fetchPattern()
-    }, [])
-
+    const fetchPatterns = useFetch(`all-patterns/get`, 'GET', currentToken)
     const loadPattern = (e) => {
         e.preventDefault()
-        const myHeaders = {
-            "Authorization": `${currentToken}`,
-            "Content-Type": "application/json"
-        };
-
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            credentials: 'include'
-        };
-
-        fetch(`http://localhost:3000/pattern/${patternToLoad}`, requestOptions)
-            .then(response => {
-                if (response.status === 401) {
-                    authenticatedToken([])
-                    navigate(`/sign-in`)
-                }
-                return response
-            })
-            .then(response => response.json())
-            .catch(error => console.log('Fetch error', error));
+        fetchHandler(`pattern/${patternToLoad}`, 'GET', currentToken)
     }
 
-    const fetchPattern = async () => {
-
-        const myHeaders = {
-            "Authorization": `${currentToken}`,
-            "Content-Type": "application/json"
-        };
-
-        const requestOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            credentials: 'include'
-        };
-
-        fetch(`http://localhost:3000/${apiRoute}`, requestOptions)
-            .then(response => {
-                if (response.status === 401) {
-                    authenticatedToken([])
-                    navigate(`/sign-in`)
-                }
-                return response
-            })
-            .then(response => response.json())
-            .then(patternList => setPatternNames(patternList))
-            .then(() => setIsFetching(false))
-            .catch(error => console.log('Fetch error', error));
-    }
+    const isListLoaded = !fetchPatterns.response
+    const patternList = fetchPatterns.response;
 
     return (
         <div className="pattern-container">
             <button
                 className={coordinatesBtn}
-                onClick={(e)=> loadPattern(e)}
+                onClick={(e) => loadPattern(e)}
             >
                 Load Pattern
             </button>
-            {isFetching
-                ? <p>Loading</p>
-                : <select
+            {isListLoaded
+                ? <p>Loading patterns from the server</p>
+                :
+                <select
                     className="pattern-select"
                     size="3"
                     onChange={e => setPatternToLoad(e.target.value)}
                 >
-                    {patternNames.listPatterns.map((option) => (
-                        <option value={option.id}>{option.patternName}</option>
+                    {patternList.map((option) => (
+                        <option key={option.id} value={option.id}>{option.patternName}</option>
                     ))}
                 </select>
             }
---
+++ src/utils/useFetch.js
@@ -0,0 +1,54 @@
+import React, {useEffect} from "react";
+import {authenticatedToken} from "./cache";
+import {navigate} from "gatsby";
+
+export const fetchHandler = async (apiRoute, method, currentToken) => {
+
+    const myHeaders = {
+        "Authorization": `${currentToken}`,
+        "Content-Type": "application/json"
+    };
+
+    const requestOptions = {
+        method: `${method}`,
+        headers: myHeaders,
+        credentials: 'include'
+    };
+    return await fetch(`http://localhost:3000/${apiRoute}`, requestOptions)
+        .then(response => {
+            if (response.status === 401) {
+                authenticatedToken([])
+                navigate(`/sign-in`)
+            }
+            return response
+        });
+}
+
+export const useFetch = (apiRoute, method, currentToken) => {
+
+    const [response, setResponse] = React.useState(null);
+    const [error, setError] = React.useState(null);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const res = await fetchHandler(`${apiRoute}`, method, currentToken);
+                const json = await res.json();
+
+                setResponse(json);
+                setIsLoading(false);
+            } catch (error) {
+                setError(error);
+            }
+        };
+
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return {response, error, isLoading};
+};
