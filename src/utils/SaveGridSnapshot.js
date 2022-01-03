db7e8ce91f19c289c13f37e0d9a4eda4726a6f28
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-01-03 20:19:16 +0100

    Create SaveGridSnapshot handler.

---
+++ src/utils/SaveGridSnapshot.js
@@ -0,0 +1,49 @@
+
+
+import {authenticatedToken} from "./cache"
+import {navigate} from "gatsby";
+
+const fetch = require(`node-fetch`)
+
+/**
+ * @param {string} api
+ * @param {string} token
+ * @param {[]} grid
+ * @param {string} creator
+ * @param {string} snapshotName
+ */
+export const saveGridSnapshot = async (api, token, creator, snapshotName, grid) => {
+    // console.log(`authenticatedToken(): ` + authenticatedToken());
+    const myHeaders = {
+        "Authorization": `${token}`,
+        "Content-Type": "application/json"
+    };
+
+    const raw = JSON.stringify({
+        "creator": creator,
+        "snapshot_name": snapshotName,
+        "pattern": grid
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        // redirect: 'follow',
+        credentials: 'include'
+    };
+
+    return fetch(`http://localhost:3000/${api}`, requestOptions)
+        .then(response => {
+            if (response.status === 401) {
+                authenticatedToken([])
+                navigate(`/sign-in`)
+            }
+            return response
+        })
+        .then(response => response.text())
+        // .then((text) => console.log(text))
+        .catch(error => console.log('Fetch error', error));
+}
+
+
