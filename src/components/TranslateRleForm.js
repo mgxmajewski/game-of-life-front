345d8bac6cf5e5c208e127e197a2f87695d9b40f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-01-27 23:56:30 +0100

    Create TranslateRleForm component.

---
+++ src/components/TranslateRleForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TranslateRleForm = (props) => {
+
+    return (
+        <div>
+            <button
+            >
+                Translate RLE
+            </button>
+        </div>
+    )
+}
+
+export default TranslateRleForm;
