8a930c64b32a6e494be97062728f482809fe8042
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-01-29 15:16:17 +0100

    Create input field for rle pattern.

--- src/components/TranslateRleForm.js
+++ src/components/TranslateRleForm.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const TranslateRleForm = (props) => {
 
+    const [rlePattern, setRlePattern] = useState('');
+
     return (
         <div>
+            <form>
+                <div>
+                    <label>
+                        Snapshot Name
+                        <input
+                            onChange={e => setRlePattern(e.target.value)}
+                            type="text"
+                            value={`${rlePattern}`}
+                            name="rle-pattern"
+                        />
+                    </label>
+                </div>
+            </form>
             <button
             >
                 Translate RLE
