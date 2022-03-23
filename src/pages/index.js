a77d2f65ffa2176205396daf2d7594d7636a1d2c
mgxmajewski <mgxmajewski@gmail.com>
2022-03-23 13:22:55 +0100

    Check auto deploy.

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => {
                 <h3>Discover Cellular Automata</h3>
                 <p>You can watch already discovered patterns or try to find new ones yourself.</p>
                 <Link className="btn" to="/grid">New Game</Link>
+                <p>popop new</p>
             </div>
         </section>
     )
