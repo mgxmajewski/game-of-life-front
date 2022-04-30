c2742ea7a19d86a8162cbb76b156934e927192cf
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-30 22:21:10 +0200

    Adjust main page styles.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {Link} from "gatsby";
-import {header} from "../styles/home.module.css";
+import {header, copyParagraph} from "../styles/home.module.css";
 import {btn} from "../styles/global.css";
 
 
@@ -10,7 +10,7 @@ const IndexPage = () => {
             <div>
                 <h2>Conway&apos;s Game of Life</h2>
                 <h3>Discover Cellular Automata</h3>
-                <p>You can watch already discovered patterns or try to find new ones yourself.</p>
+                <p className={copyParagraph}>You can watch already discovered patterns or try to find new ones yourself.</p>
                 <Link className="btn" to="/grid">New Game</Link>
             </div>
         </section>
