c62fe063c1350ca83c4efe3da186863b99c3ca91
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-01 16:31:56 +0200

    Create navbar css module and add some padding.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,30 +2,46 @@ import React from 'react';
 // import {Link} from "gatsby";
 import {handleLogout} from "../utils/LogoutHandler";
 import {navigate} from "gatsby";
+import {hellouser, links, loginOrOut, logo} from "../styles/navbar.module.css"
 
 
 const NavBar = () => {
 
-const userName = typeof window !== "undefined"
-    ? localStorage.getItem('userName')
-    : null;
-const userLogged = userName !== null;
+    const userName = typeof window !== "undefined"
+        ? localStorage.getItem('userName')
+        : null;
+    const userLogged = userName !== null;
 
     return (
         <nav>
-            <h1 className="logo" onClick={() => navigate('/')}>AgileCat.io</h1>
-            <div className="links">
+            <h1
+                className={logo}
+                onClick={() => navigate('/')}
+            >
+                AgileCat.io
+            </h1>
+            <div className={links}>
                 {/*<Link to="/">Home</Link>*/}
                 {/*<Link to="/grid">Grid</Link>*/}
                 {userLogged ?
-                    <React.Fragment>
-                        <p className="hellouser">Hi, {userName}!</p>
-                        <p className="login-or-out" onClick={() => handleLogout()}>Sign Out</p>
-                    </React.Fragment>
+                    <>
+                        <p className={hellouser}>Hi, {userName}!</p>
+                        <p
+                            className={loginOrOut}
+                            onClick={() => handleLogout()}
+                        >
+                            Sign Out
+                        </p>
+                    </>
                     :
-                    <React.Fragment>
-                        <p className="login-or-out" onClick={() => navigate('/sign-in')}>Login</p>
-                    </React.Fragment>
+                    <>
+                        <p
+                            className={loginOrOut}
+                            onClick={() => navigate('/sign-in')}
+                        >
+                            Login
+                        </p>
+                    </>
                 }
             </div>
         </nav>
