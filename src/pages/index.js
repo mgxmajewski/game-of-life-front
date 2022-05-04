b9a4848c0b07161155a8b17891eaac41b74008f5
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-04 20:08:35 +0200

    Add copy and adjust style of the project.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,100 @@
 import * as React from "react";
 import {Link} from "gatsby";
-import {header, copyParagraph} from "../styles/home.module.css";
-import {genericButton} from "../styles/global.module.css";
+import {
+    embeddedVideoContainer,
+    header,
+    headerButton,
+    leadCopyParagraph,
+    mainCopyParagraph
+} from "../styles/home.module.css";
+import {
+    genericButton,
+    simpleSeparator,
+    tabSeparator,
+    tabSeparator__line,
+    tabSeparator__tab,
+    tabSeparatorHigher,
+    tabSeparatorMediumHigher
+} from "../styles/global.module.css";
 import YoutubeEmbed from "../components/YoutubeEmbed";
 
 
 const IndexPage = () => {
-    return (
-        <section className={header}>
-            <div>
+    return (<>
+            <section className={`${header}`}>
                 <h2>Conway&apos;s Game of Life</h2>
-                <h3>Discover Cellular Automata</h3>
-                <p className={copyParagraph}>You can watch already discovered patterns or try to find new ones yourself.</p>
-                <Link className={genericButton} to="/grid">New Game</Link>
-                <YoutubeEmbed embedId={`HeQX2HjkcNo?start=62`}/>
-            </div>
-        </section>
+                <h3>Discover the famous cellular automaton</h3>
+                <Link
+                    className={`${genericButton} ${headerButton}`}
+                    to="/grid">
+                    Setup your own Game of Life
+                </Link>
+            </section>
+            <section>
+                <div className={`${simpleSeparator} ${tabSeparatorHigher}`}></div>
+                <p className={leadCopyParagraph}>
+                    TL;DR If you have never heard about cellular automata, don't worry!
+                    You can click the button above and tinker with the game of life without any knowledge. Curious? Read
+                    more below.
+                </p>
+                <div className={simpleSeparator}></div>
+                <div className={`${tabSeparator} ${tabSeparatorMediumHigher}`}>
+                    <div className={tabSeparator__tab}>cellular automata</div>
+                    <div className={tabSeparator__line}></div>
+                </div>
+                <p className={mainCopyParagraph}>
+                    There is an alternative name for cellular automaton, iterative arrays which gives more intuition
+                    about
+                    what it is. A most basic simplified explanation would be that it is a set of rules describing how
+                    cells
+                    of a 2-dimensional grid change their state in time (more precisely discrete iterations). There are
+                    many
+                    types of cellular automaton and Conway's Game of Life is one of them but the whole concept was
+                    originally discovered in the 1940s by Stanisław Ulam and John von Neumann (Manhattan Project).
+                    Another
+                    famous cellular automaton is Rule 30.
+                    More on the topic you can find in Stephen Wolfram's (creator of Wolfram Mathematica) book A New Kind
+                    of
+                    Science or interview with hum by Lex Fridman(link to interview).
+                </p>
+                <div className={simpleSeparator}></div>
+                <div className={`${tabSeparator} ${tabSeparatorMediumHigher}`}>
+                    <div className={tabSeparator__tab}>game of life</div>
+                    <div className={tabSeparator__line}></div>
+                </div>
+                <p className={mainCopyParagraph}>
+                    Back to Conway's Game of Life. It is a relatively simple set of rules.
+                    First of all, cells can be either dead or alive and their behavior over time is*:
+                    <ol>
+                        <li>Any live cell with two or three live neighbors survives.</li>
+                        <li>Any dead cell with three live neighbors becomes a live cell.</li>
+                        <li>All other live cells die in the next generation. Similarly, all other dead cells stay
+                            dead.
+                        </li>
+                    </ol>
+                    That's all. Once you set up the initial state of the grid the rest is a matter of the iterating
+                    changes
+                    according to the rules. After the start, some really interesting things start to happen. Some of the
+                    <div className={embeddedVideoContainer}><YoutubeEmbed embedId={`HeQX2HjkcNo?start=62`}/></div>
+                    initial states die soon, but others keep generating themselves indefinitely or they get into an
+                    oscillation state, eventually, the pattern gets chaotic. Despite the simplicity of the rules, the
+                    result of the game is undecidable. There is no way to predict the behavior of the grid other than
+                    running
+                    it over time which is pretty significant keeping in mind that we can predict so many much more
+                    complex
+                    systems existing in nature. This has really deep mathematical meaning and is described in the
+                    attached video. I highly recommend watching the whole episode, but the link includes a time mark to
+                    fragment
+                    about the game of life.
+                </p>
+
+                <p className={mainCopyParagraph}>
+                    Another interesting video is an interview with John Conway, an English mathematician who invented
+                    the
+                    Game of Life (link to the interview).
+                </p>
+            </section>
+        </>
     )
 }
 
