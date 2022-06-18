9ee5b79080df41159effbf1965844f51f8a597e2
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-18 21:11:56 +0200

    Remove unused import.

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 import GridProvider from "../components/GridProvider";
 import AuthSync from "../components/AuthSync";
-import {uiLayout} from'../styles/grid-editor-ui-layout.module.css'
-import {gradientBackgroundWithShadowAlter} from"../styles/global.module.css"
+import {uiLayout} from '../styles/grid-editor-ui-layout.module.css'
 
 const CanvasGridPageWrapped = () => {
     return (
